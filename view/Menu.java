dfb7e5782465bb1fe019ed0d172cf429703182e8
YinTingyu <122711888+YinTingyu@users.noreply.github.com>
2023-04-26 08:20:25 -0400

    Add files via upload
    all the gui files done

---
+++ view/Menu.java
@@ -0,0 +1,14 @@
+package view;
+
+public class Menu {
+    static final String BLOCK_LIST = "Block List";
+    static final String INVISIBLE_LIST = "Invisible List";
+    static final String REPEAT_BLOCK = "The user is already in the block list";
+    static final String REPEAT_INVISIBLE = "The user is already in invisible list!";
+    static final String NO_RESULT = "No results for ";
+    static final String INVISIBLE_WARNING = "The user is invisible";
+    static final String FOUND_USER = "We found the user that you search for: ";
+    static final String BLOCK_SUCCEED = "Successfully blocked this user!";
+    static final String INVISIBLE_SUCCEED = "Successfully become invisible to this user!";
+    static final String LOG_OUT = "Log out";
+}
