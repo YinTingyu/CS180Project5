b90a9fb3fb49e1e921c5ce72bbdf20a2d54850c3
YinTingyu <122711888+YinTingyu@users.noreply.github.com>
2023-04-30 17:22:23 -0400

    Update SellerMenu.java

--- view/SellerMenu.java
+++ view/SellerMenu.java
@@ -2,7 +2,6 @@ package view;
 
 import core.Customer;
 import core.Seller;
-import core.Store;
 import utils.CSVReader;
 import utils.CSVWriter;
 
@@ -13,87 +12,72 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 /**
- * A seller menu
+ * A customer menu
  *
  * <p>Purdue University -- CS18000 -- Spring 2023 -- project 5
  *
  * @author Tingyu Yin
- * @version April 19, 2023
+ * @version April 15, 2023
  */
-public class SellerMenu extends Menu {
-    private static final String MANAGE_STORE = "Manage my store";
+public class CustomerMenu extends Menu {
+
+    private static final String VIEW_ALL_STORES = "View all the stores";
 
     public String message;
-    private String filename = "./src/sellers.csv";
-    private SellerMenu sellerMenu;
+    private String filename = "./src/customers.csv";
     private CSVReader csvReader;
-
-    public Seller seller;
+    public Customer customer;
     public List<String> blockList;
     public List<String> invisList;
     public List<String> sellerList = new ArrayList<>();
-    public List<Store> sellerStore;
     private JPanel invisiblePanel = new JPanel();
     private JPanel blockPanel = new JPanel();
 
-    public void showSellerMenu(Seller seller) throws IOException { // this is used when login and when go back
-        run(this.seller);
+
+
+    public void showCustomerMenu(Customer customer) throws IOException { // this is used when login and when go back
+        run(customer);
     }
 
-    public SellerMenu(Seller seller) { // used when login gui instance the CustomerMenu
-        this.seller = seller;
+    public CustomerMenu(Customer customer) { // used when login gui instance the CustomerMenu
+        this.customer = customer;
         this.csvReader = new CSVReader();
-        try {
-            sellerStore = csvReader.getSellerStores(seller);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+
     }
 
-    public void openSellerSMGWindow(SellerMenu sellerMenu, Customer customer, Seller seller) {
-        SellerSMGWindow sendWindow = new SellerSMGWindow(sellerMenu, customer, seller);
-        try {
-            sendWindow.run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void openManageStoresWindow(Seller seller) throws IOException { // to open view all the stores GUI
-        ManageStoresWindow storesWindow = new ManageStoresWindow(() -> { // use lambda expressions to create anonymous methods
+    public void openViewStoresWindow(Customer customer) throws IOException { // to open view all the stores GUI
+        ViewStoresWindow storesWindow = new ViewStoresWindow(() -> { // use lambda expressions to create anonymous methods
             try {
-                showSellerMenu(seller);
+                showCustomerMenu(customer);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }, this.seller, this); // the lambda expression is passed as the first argument,
-        // and the Customer object is passed as the second argument, sellerMenu as the third parameter
+        }, this.customer, this); // the lambda expression is passed as the first argument,
+        // and the Customer object is passed as the second argument
         storesWindow.run();
     }
 
-    public void run(Seller seller) throws IOException {
-        CSVWriter writer = new CSVWriter(seller);
+    public void run(Customer customer) throws IOException {
 
-        this.seller = seller;
-        sellerStore = seller.getStores();
-        Map<String, Customer> customerMap = csvReader.readCustomers();
+        CSVWriter writer = new CSVWriter(customer);
 
-        for (String customer : customerMap.keySet()) {
-            sellerList.add(customer);
+        Map<String, Seller> sellerMap = csvReader.readSellers();
+
+        for (String seller : sellerMap.keySet()) {
+            sellerList.add(seller);
         }
 
-        blockList = csvReader.getBlockList(seller); // load all the blocked users
-        invisList = csvReader.getInvisList(seller); // load all the invisible users
+        blockList = csvReader.getBlockList(customer); // load all the blocked users
+        invisList = csvReader.getInvisList(customer); // load all the invisible users
 
-        JFrame frame = new JFrame("Seller Menu");
+        JFrame frame = new JFrame("Customer Menu");
         frame.setSize(new Dimension(500, 600));
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
 
@@ -103,7 +87,7 @@ public class SellerMenu extends Menu {
         JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JTextField searchField = new JTextField(25);
         searchField.setEditable(false);
-        searchPanel.add(new JLabel("Search for"));
+        searchPanel.add(new JLabel("Search user"));
         searchPanel.add(searchField);
         mainPanel.add(searchPanel, BorderLayout.NORTH);
 
@@ -120,7 +104,7 @@ public class SellerMenu extends Menu {
             public void actionPerformed(ActionEvent actionEvent) {
                 String searchUsername = searchField.getText();
                 List<String> searUserInvisList = new ArrayList<>();
-                Customer searched = customerMap.get(searchUsername);
+                Seller searched = sellerMap.get(searchUsername);
                 if (searched == null) { // if searched user is null then no results
                     JOptionPane.showMessageDialog(null, NO_RESULT + searchUsername,
                             null, JOptionPane.ERROR_MESSAGE);
@@ -130,7 +114,7 @@ public class SellerMenu extends Menu {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                    if (searUserInvisList.contains(seller.getUsername())) { // check if the seller in the searched user's invisible list
+                    if (searUserInvisList.contains(customer.getUsername())) { // check if the customer in the user's invisible list
                         JOptionPane.showMessageDialog(null, INVISIBLE_WARNING,
                                 null, JOptionPane.PLAIN_MESSAGE);
                     } else {
@@ -171,7 +155,7 @@ public class SellerMenu extends Menu {
 
                     // (write) update csv file
                     try {
-                        writer.writeBlockList(seller, blockList);
+                        writer.writeBlockList(customer, blockList);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -195,24 +179,9 @@ public class SellerMenu extends Menu {
 
                     // (write) update csv file
                     try {
-                        writer.writeInvisList(seller, invisList);
+                        writer.writeInvisList(customer, invisList);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
-                    }
-                }
-            });
-
-            JButton sendMSGButton = new JButton("Send Message");
-            friendButtonPanel.add(sendMSGButton);
-            sendMSGButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-
-                    if (blockList.contains(seller.getUsername())) {
-                        JOptionPane.showMessageDialog(null, "You have been blocked!",
-                                "Error", JOptionPane.ERROR_MESSAGE);
-                    } else {
-                        openSellerSMGWindow(sellerMenu, customerMap.get(friend), seller);
                     }
                 }
             });
@@ -225,25 +194,24 @@ public class SellerMenu extends Menu {
         JScrollPane scrollPane = new JScrollPane(friendPanel);
         mainPanel.add(scrollPane, BorderLayout.CENTER);
 
-        // bottom panel to hold bottom buttons
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton viewBlockList = new JButton(BLOCK_LIST);
         JButton viewInvisibleList = new JButton(INVISIBLE_LIST);
-        JButton manageStores = new JButton(MANAGE_STORE); // manage stores
+        JButton viewAllStores = new JButton(VIEW_ALL_STORES);
         JButton logOutButton = new JButton(LOG_OUT);
-        bottomPanel.add(manageStores);
+        bottomPanel.add(viewAllStores);
         bottomPanel.add(viewBlockList);
         bottomPanel.add(viewInvisibleList);
         bottomPanel.add(logOutButton);
 
         mainPanel.add(bottomPanel, BorderLayout.SOUTH);
 
-        manageStores.addActionListener(new ActionListener() {
+        viewAllStores.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     frame.dispose();
-                    openManageStoresWindow(seller);
+                    openViewStoresWindow(customer);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -253,25 +221,22 @@ public class SellerMenu extends Menu {
         viewBlockList.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                showBlockDialog(seller);
+                showBlockDialog(customer);
             }
         });
 
         viewInvisibleList.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                showInvisibleDialog(seller);
+                showInvisibleDialog(customer);
             }
         });
 
         logOutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                // get timestamp when log out
-                Timestamp logOutTimestamp = new Timestamp(System.currentTimeMillis());
 
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String logOutTspStr = dateFormat.format(logOutTimestamp);
+
                 frame.dispose();
             }
         });
@@ -284,15 +249,17 @@ public class SellerMenu extends Menu {
 
     // have to separately refresh the block panel
     private void updateBlockPanel(List<String> blockList) {
+        CSVWriter writer = new CSVWriter(customer);
 
-        CSVWriter writer = new CSVWriter(seller);
         blockPanel.removeAll();
 
         if (blockList.isEmpty()) {
+
             JLabel emptyMessage = new JLabel("Block list is empty");
             emptyMessage.setHorizontalAlignment(SwingConstants.CENTER);
             blockPanel.add(emptyMessage);
         } else {
+
             for (String block : blockList) {
                 JPanel singleBlockPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
                 JLabel blockLabel = new JLabel(block);
@@ -313,7 +280,7 @@ public class SellerMenu extends Menu {
 
                         // (write) update csv file
                         try {
-                            writer.writeBlockList(seller, blockList);
+                            writer.writeBlockList(customer, blockList);
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
@@ -328,8 +295,7 @@ public class SellerMenu extends Menu {
 
 
     // method to show block list
-    private void showBlockDialog(Seller seller) {
-
+    private void showBlockDialog(Customer customer) {
         JDialog blockDialog = new JDialog();
         blockDialog.setLayout(new BorderLayout());
         blockDialog.setSize(new Dimension(300, 400));
@@ -340,14 +306,15 @@ public class SellerMenu extends Menu {
         updateBlockPanel(blockList);
 
 
+
         JScrollPane scrollPane = new JScrollPane(blockPanel);
         blockDialog.add(scrollPane, BorderLayout.CENTER);
         blockDialog.setVisible(true);
     }
 
     public void updateInvisiblePanel(List<String> invisList) {
+        CSVWriter writer = new CSVWriter(customer);
 
-        CSVWriter writer = new CSVWriter(seller);
         invisiblePanel.removeAll();
 
         if (invisList.isEmpty()) {
@@ -375,11 +342,10 @@ public class SellerMenu extends Menu {
 
                         // (write) update csv file
                         try {
-                            writer.writeInvisList(seller, invisList);
+                            writer.writeInvisList(customer, invisList);
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
-
                     }
                 });
 
@@ -390,7 +356,7 @@ public class SellerMenu extends Menu {
     }
 
     // method to show block list
-    private void showInvisibleDialog(Seller seller) {
+    private void showInvisibleDialog(Customer customer) {
 
         JDialog invisibleDialog = new JDialog();
         invisibleDialog.setTitle("Become invisible to");
@@ -407,5 +373,4 @@ public class SellerMenu extends Menu {
 
         invisibleDialog.setVisible(true);
     }
-
 }
