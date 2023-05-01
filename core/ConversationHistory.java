5e466992b9bbecba2a1633e76df52a57de56c2fd
YinTingyu <122711888+YinTingyu@users.noreply.github.com>
2023-04-30 21:07:43 -0400

    Add files via upload
    add new file

---
+++ core/ConversationHistory.java
@@ -0,0 +1,50 @@
+package core;
+
+import java.util.*;
+
+/**
+ * A class to record all the user's conversation history in this application.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2023 -- project 4
+ *
+ * @author Tingyu Yin
+ * @version April 8, 2023
+ *
+ * April 10, 2023 - edited to include a string for the csv file for each conversation object
+ */
+public class ConversationHistory {
+    private List<Message> messages;
+    private Map<String, List<Message>> conversationMap = new HashMap<>();
+    private String filename;
+
+    public ConversationHistory(List<Message> messages, String filename) {
+        this.filename = filename;
+        this.messages = messages;
+        conversationMap.put(filename, messages);
+    }
+
+    public List<Message> getMessagesHis() {
+        return messages;
+    }
+
+
+    public List<String> getFilenames() {
+        Set<String> filenamesSet = conversationMap.keySet();
+        List<String> conFilenames = new ArrayList<>();
+        conFilenames.addAll(filenamesSet);
+        return conFilenames;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public void addMessage(Message message)
+    {
+        messages.add(message);
+    }
+
+    public void setMessagesHis(List<Message> messages) {
+        this.messages = messages;
+    }
+}
